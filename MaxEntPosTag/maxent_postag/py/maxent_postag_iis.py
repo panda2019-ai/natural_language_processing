a5d0055b7dc6ef959c512a82613a79b4e51cce6e
baiziyuandyufei <baiziyuandyufei@126.com>
2019-11-25 16:52:19 +0800

    update maxent

--- MaxEntPosTag/maxent_postag/py/maxent_postag_iis.py
+++ MaxEntPosTag/maxent_postag/py/maxent_postag_iis.py
@@ -10,35 +10,39 @@ class MaxEnt(object):
         self.feats = defaultdict(int)
         # 文本经过特征抽取后建成的训练集每一项用一个事件表示(词性标记,特征1,特征2,特征3)
         self.trainset = []
-        # 词性标记集
+        # 词性标记集，表示Y可取的值的集合，比如名词、动词、形容词...
         self.labels = set()
+        # 存储v(X=x)的次数，这里的x指的是一个事件中的特征值序列，比如(当前词，前词，后词）
+        self.vx_ = defaultdict(int)
 
-    def generate_events(self, line, train_flag=False):
-        """
-        输入一个以空格为分隔符的已分词文本，返回生成的事件序列
-        :param line: 以空格为分隔符的已分词文本
-        :param train_flag: 真时为训练集生成事件序列；假时为测试集生成事件
-        :return: 事件序列
-        """
+    # 对一行文本抽取多个事件序列
+    def generate_events(self, text, train_flag=False):
+        # 定义事件序列，注意输入的1行文本可以抽取出多个事件
         event_li = []
-        # 分词
-        word_li = line.split()
-        # 为词语序列添加头元素和尾元素，便于后续抽取事件
+        # 分词，要求输入的字符串中词语之间以空白分隔，对于训练集在词语后还要加词性标记：word/pos
+        word_li = text.split()
+        # 分离词语和词性
         if train_flag:
+            # 对于含有词性标记的词语序列，分离词语和词性：[(word1,pos1),(word2,pos2),...]
             word_li = [tuple(w.split(u'/')) for w in word_li if len(w.split(u'/')) == 2]
         else:
+            # 对于没有词性标记的词语序列，添加词性"x_pos"：[(word1,'x_pos'),(word2,'x_pos'),...]
             word_li = [(w, u'x_pos') for w in word_li]
+        # 为词语序列添加头元素和尾元素，便于后续抽取事件
         word_li = [(u'pre1', u'pre1_pos')] + word_li + [(u'pro1', u'pro1_pos')]
-        # 每个中心词抽取1个event，每个event由1个词性标记和多个特征项构成
+        # 遍历中心词抽取1个event，每个event由1个词性标记和多个特征项构成
         for i in range(1, len(word_li) - 1):
-            # 特征函数a 中心词
+            # 特征函数1 中心词
             fea_1 = word_li[i][0]
-            # 特征函数b 前一个词
+            # 特征函数2 前一个词
             fea_2 = word_li[i - 1][0]
-            # 特征函数d 下一个词
-            fea_4 = word_li[i + 1][0]
-            # 构建一个事件
-            fields = [word_li[i][1], fea_1, fea_2, fea_4]
+            # 特征函数3 后一个词
+            fea_3 = word_li[i + 1][0]
+            # 词性y
+            y = word_li[i][1]
+            # 构建1个事件，注意1个事件由3个特征项构成，同一事件中的3个特征项共享1个输出标记y
+            # 因此1个事件对应3个特征函数：f1(fea_1,y),f2(fea_2,y),f3(fea_3,y)
+            fields = [y, fea_1, fea_2, fea_3]
             # 将事件添加到事件序列
             event_li.append(fields)
         # 返回事件序列
@@ -51,78 +55,66 @@ class MaxEnt(object):
                     break
                 line = line.strip()
                 if line:
-                    # 生成事件序列
+                    # 抽取事件序列
                     events_li = self.generate_events(line, train_flag=True)
-                    for fields in events_li:
-                        # 第1列是标记
-                        label = fields[0]
-                        # 添加标记
+                    # 遍历每个事件，对每个事件构建3个特征函数fi(x,y)
+                    for event in events_li:
+                        # 第1列是输出词性标记y
+                        label = event[0]
+                        # 更新词性标记
                         self.labels.add(label)
-                        # 更新(标记，特征)计数
-                        for f in set(fields[1:]):
-                            # 更新p_wan(t, x)的数量，注意是（标记，特征）构成1种特征函数，而不是(标记,特征序列)构成1种特征函数
+                        # 对(词性标记，特征)计数，以便后续计算P(X,Y)的经验分布
+                        for f in set(event[1:]):
                             self.feats[(label, f)] += 1
-                        # 存储event
-                        self.trainset.append(fields)
+                        self.px_[tuple(event[1:])] += 1
+                        # 将event添加到训练集
+                        self.trainset.append(event)
 
+    # 初始化模型参数
     def _initparams(self):
-        """
-        初始化模型参数
-        :return: 无
-        """
         # 训练集中抽取的事件总数
-        self.size = len(self.trainset)
-        # 1个事件中所含特征数量的最大值，此示例中该特征数量为定值
+        self.N = len(self.trainset)
+        # 1个事件对应的特征数量为定值，因此1个事件对应的特征函数数量也为定值
         self.M = max([len(record) - 1 for record in self.trainset])
-        # 期望值序列初始化，每一种特征函数都对应一个经验值
+        # 计算每一种特征函数fi(x,y)关于经验分布P_(X,Y)的期望值，指的是《统计学习方法》P82页最下边的式子
         self.ep_ = [0.0] * len(self.feats)
-        # 遍历每一个特征函数
+        # 遍历每一个特征函数，注意这里的f对应的是(label,特征)这个元组
         for i, f in enumerate(self.feats):
             # 第i个特征函数的经验值=特征(x,y)出现次数/样本数量。注：这里认为一个event中没有重复的特征
-            self.ep_[i] = float(self.feats[f]) / float(self.size)
-            # 记录特征函数索引
+            self.ep_[i] = float(self.feats[f]) / float(self.N)
+            # 记录特征函数索引，注意这里的f对应的是(label,特征)这个元组，
+            # 经过这一步后self.feats中记录的是特征函数的索引值，以便后续预测时读取特征函数对应的权值
             self.feats[f] = i
-        # 权重序列初始化为全0，每一个特征函数都对应一个权重值。就是所求的系数
+        # 权重序列初始化为全0，每一个特征函数都对应一个权重值，就是所求的系数
         self.w = [0.0] * len(self.feats)
         self.lastw = self.w
 
-    def probwgt(self, features, label):
-        """
-        计算指数族的值
-        :param features: 一个事件的特征序列
-        :param label: 期待输出的标记
-        :return: p(标记label|1个事件的特征序列)
-        """
-        wgt = 0.0
-        for f in features:
-            if (label, f) in self.feats:
-                wgt += self.w[self.feats[(label, f)]]
-        return math.exp(wgt)
-
-    # calculate feature expectation on model distribution
+    # 计算每一种特征函数fi(x,y)关于模型与经验分布P_(X)的期望值，指的是《统计学习方法》P83页最上边的式子
     def Ep(self):
+        # 期望值的个数等于特征函数的个数
         ep = [0.0] * len(self.feats)
+        # 遍历每个事件，目的是遍历P83页式子中的所有x
         for record in self.trainset:
+            # 分离出特征值
             features = record[1:]
-            # calculate p(y|x)
-            prob = self.calprob(features)
+            # 根据特征值计算对应的每种标记y的概率，实际上表示出式子中的P(y|x)
+            # 显然针对一个事件，只有一个p(y|x)
+            prob_li = self.calprob(features)
+            # 遍历每一个特征值
             for f in features:
-                for w, l in prob:
-                    # only focus on features from training data.
-                    if (l, f) in self.feats:
-                        # get feature id
-                        idx = self.feats[(l, f)]
-                        # sum(1/N * f(y,x)*p(y|x)), p(x) = 1/N
-                        ep[idx] += w * (1.0 / self.size)
+                # 遍历每个标记和其对应的概率值
+                for prob, label in prob_li:
+                    # 由标记和特征构建出1个特征函数
+                    if (label, f) in self.feats:
+                        # 读取出特征函数的索引值
+                        idx = self.feats[(label, f)]
+                        # P(x)的经验分布计算 p(x)=1/N
+                        ep[idx] += prob * (self.px_[tuple(features)]*1.0 / self.N)
         return ep
 
+    # 检查权值向量是否收敛，收敛条件为W中的每个元素都不再发生变化，
+    # 收敛返回True，否则返回False
     def _convergence(self, lastw, w):
-        """
-        判断训练是否已经收敛，收敛条件为W中的每个元素都不再发生变化
-        :param lastw: 原来的w序列值
-        :param w: 当前的w序列值
-        :return: 是否收敛的标志
-        """
         for w1, w2 in zip(lastw, w):
             if abs(w1 - w2) >= 0.01:
                 return False
@@ -137,26 +129,40 @@ class MaxEnt(object):
         """
         # 初始化模型参数
         self._initparams()
+        # 开始迭代
         for i in range(max_iter):
             print('iter %d ...' % (i + 1))
             # 计算特征函数关于模型P(Y|X)和经验分布P(X)的期望值序列，每个特征函数对应一个值
-            self.ep = self.Ep()
+            ep = self.Ep()
+            # 保存当前的权值向量
             self.lastw = self.w[:]
+            # 计算delta并更新权值向量
             for i, w in enumerate(self.w):
-                delta = 1.0 / self.M * math.log(self.ep_[i] / self.ep[i])
-                # 更新参数
+                # 计算delta
+                delta = 1.0 / self.M * math.log(self.ep_[i] / ep[i])
+                # 更新权值
                 self.w[i] += delta
             # 检查是否收敛
             if self._convergence(self.lastw, self.w):
                 break
 
+    # 最大熵模型的预测函数P(y|x),第1个输入为1个事件中的特征x，可以对应3个特征函数f1(x,y),f2(x,y),f3(x,y)
+    # features对应特征x=(x1,x2,x3)，第2个输入为label，它对应输出标记y，输出为P(y|x)的未归一化的概率值
+    def probwgt(self, features, label):
+        wgt = 0.0
+        # 遍历每个特征f
+        for f in features:
+            # 如果训练集中见过特征函数(y,f)则读出权重
+            if (label, f) in self.feats:
+                # 累加权重
+                wgt += self.w[self.feats[(label, f)]]
+        # 取exp(权重和)作为预测值，这里没有进行归一化
+        return math.exp(wgt)
+
+    # 最大熵模型的预测函数P(y|x)，输入为1个事件中的特征x，输出为标记集中每种标记对应的归一化的概率P(y1|x),P(y2|x),...
+    # 返回[(p1, y1),(p2,y2),...]
     def calprob(self, features):
-        """
-        计算条件分布p(y|x)，即特征序列x关于每个可能的标记y的概率值
-        :param features: 1个事件的多个特征序列
-        :return:返回条件分布
-        """
-        # 计算事件中所有特征关于各标记的指数族的值
+        # 计算标记集中每种标记的条件概率
         wgts = [(self.probwgt(features, l), l) for l in self.labels]
         # 使每个概率值相加后等于1
         Z = sum([w for w, l in wgts])
